bfc00752504936dd3a2e0507c1b34983f3351e28
Nicoam89 <nico.am98@gmail.com>
2026-02-03 10:36:09 -0300

    credenciales mongo

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ export const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB conectado");
   } catch (error) {
-    console.error("❌ Error conectando MongoDB:", error);
+    console.error("❌ Error Mongo:", error);
     process.exit(1);
   }
 };
