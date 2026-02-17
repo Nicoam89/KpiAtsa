73864094e4691717a1b5c4d92d7eb3f58992bb56
Nicoam89 <nico.am98@gmail.com>
2026-02-17 09:45:43 -0300

    se corrigen urls

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -24,7 +24,7 @@ export default function ChangePassword() {
     try {
       setLoading(true);
 
-      const res = await fetch("http://localhost:3000/api/auth/change-password", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
